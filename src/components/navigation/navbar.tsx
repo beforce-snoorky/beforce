fdd5701bcfb2ab03d7717d08bfe9a25f6113a131
Raphael Vitor <raphaelvitorlopes@icloud.com>
2025-07-26 17:13:32 -0300

    refactor(navbar): remove unused billing route and improve mobile layout

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Company } from "@/types/company"
-import { Globe2, Headphones, LayoutDashboard, MonitorSmartphone, Receipt, Server, SlidersHorizontal } from "lucide-react"
+import { Globe2, Headphones, LayoutDashboard, MonitorSmartphone, Server, SlidersHorizontal } from "lucide-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import toast, { Toaster } from "react-hot-toast"
@@ -23,7 +23,6 @@ export function NavBar({ company }: { company: Company }) {
     { icon: <Headphones className="w-5 h-5" />, name: "WhatsApp", url: "/dashboard/support", visible: company.has_whatsapp },
     { icon: <Globe2 className="w-5 h-5" />, name: "Website", url: "/dashboard/analytics", visible: company.has_website },
     { icon: <Server className="w-5 h-5" />, name: "Sistemas", url: "/dashboard/systems", visible: company.has_management_system },
-    { icon: <Receipt className="w-5 h-5" />, name: "Boletos", url: "/dashboard/billing", visible: false },
     { icon: <MonitorSmartphone className="w-5 h-5" />, name: "Marketing", url: "/dashboard/marketing", visible: company.has_mkt_digital },
   ];
 
@@ -31,8 +30,8 @@ export function NavBar({ company }: { company: Company }) {
     <>
       <Toaster position="bottom-center" />
 
-      <aside className="xl:hidden fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50 flex items-center gap-1 text-sm">
-        <div className="flex items-center justify-between p-1 rounded-full shadow-sm border border-surface bg-light">
+      <aside className="xl:hidden fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50 px-4 min-w-sm w-full flex items-center gap-1 text-sm">
+        <div className="flex flex-1 items-center justify-between p-1 rounded-full shadow-sm border border-surface bg-light">
           {routes.map((item, index) => {
             const isActive = pathname === item.url
 
@@ -48,7 +47,7 @@ export function NavBar({ company }: { company: Company }) {
                 key={index}
                 href={item.visible ? item.url : "#"}
                 onClick={handleClick}
-                className={`relative z-10 flex items-center gap-2 py-2 px-3 rounded-full font-medium
+                className={`relative z-10 flex items-center justify-center grow gap-2 py-2 px-3 rounded-full font-medium
                   ${isActive && ("bg-accent text-light")} ${!item.visible && ("cursor-not-allowed bg-gray-200 text-gray-400 mx-1")}
                 `}
                 tabIndex={item.visible ? 0 : -1}
