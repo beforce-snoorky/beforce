7793556d78e9bc7f7bdb7131861b68ddd2c210c8
Raphael Vitor <raphaelvitorlopes@icloud.com>
2025-07-31 10:35:03 -0300

    refactor: move text-dark styling from globals.css to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-BR">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased text-dark">{children}</body>
     </html>
   )
 }
